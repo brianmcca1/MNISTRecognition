ec155cec2b79357f606b2fb26ee02b4d122bb67c
Connor Weeks <cweeks314@gmail.com>
2018-02-27 19:05:46 -0500

    Added way to visualize knn classification errors

--- knn.py
+++ knn.py
@@ -3,6 +3,8 @@ from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 import prepare_data
 
+IMAGE_SIZE = 28
+
 # load data set
 (training_images, training_labels,
  validation_images, validation_labels,
@@ -22,10 +24,21 @@ prediction = classifier.predict(test_images)
 num_correct = 0
 confusion_matrix = np.zeros(shape=[10, 10])
 
+# Change to true if you want to view incorrectly classified images
+SHOW_INCORRECT = False
+if SHOW_INCORRECT:
+    from matplotlib import pyplot as plt
+
 for i in range(len(prediction)):
     confusion_matrix[prediction[i], test_labels[i]] += 1
     if prediction[i] == test_labels[i]:
         num_correct += 1
+    elif SHOW_INCORRECT:
+        # show incorrect results
+        plt.imshow(np.reshape(test_images[i], [IMAGE_SIZE, IMAGE_SIZE]))
+        plt.show()
+        print(test_labels[i], "classified as", prediction[i])
+        input()
 
 print("Accuracy: " + str(num_correct / len(prediction)))
 print("KNN CONFUSION MATRIX:")
